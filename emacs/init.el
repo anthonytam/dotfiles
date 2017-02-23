18cf1e4503b664ceb20948196f2955b666e5afa7
Anthony Tam <anthony@anthonytam.me>
2017-02-23 00:26:17 -0500

    Added JS to web mode in emacs Some adjustments to i3 and compton Changes to i3 bar

--- emacs/init.el
+++ emacs/init.el
@@ -88,12 +88,15 @@
 (use-package web-mode)
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+; Small JS tweak
+(setq web-mode-content-types-alist
+      '(("jsx" . "\\.js[x]?\\'")))
 
 (use-package flycheck
   :config
