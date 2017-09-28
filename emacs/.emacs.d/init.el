803e7a0e9699693498653ece710a3a7b4faa3aae
Josh Wolfe <jwolfe@indigo.ca>
2017-09-28 12:02:06 -0400

    Supress package.el message in init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,12 +8,7 @@
 ;;
 
 ;; Set garbage collect high to speed up startup
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (let ((gc-cons-threshold most-positive-fixnum))
+  ;; This stops emacs from appending (package-initialize) to this file
+  (setq package--init-file-ensured t)
   (org-babel-load-file "~/.emacs.d/README.org"))
