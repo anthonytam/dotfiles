7dc5f1cb6fb98af586ca51122a46e1ac1059c494
Anthony Tam <anthony.tam@controlgap.com>
2026-08-09 23:17:40 -0400

    Add ADE-MBP

---
+++ zsh/.zsh/host-specific/ADE-MBP-FVWKH4LH72.sh
@@ -0,0 +1,7 @@
+export SSH_AUTH_SOCK="$HOME/.bitwarden-ssh-agent.sock"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
